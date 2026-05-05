1eb8dbc7367ecaf51e9174cf1f528d7a44a6da24
Derek <derek@example.com>
2026-05-04 21:20:49 -0500

    isolation test removing analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { Analytics } from '@vercel/analytics/react';
 
-
+// We are completely removing Analytics for this test to see if it is causing the crash
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics /> {/* This must be used as a tag, inside the tree! */}
     </BrowserRouter>
   </React.StrictMode>
 );
