bf7875cda739a8a25956f0f7781afb0ae4ef5a2f
Derek <derek@example.com>
2026-05-04 21:26:40 -0500

    restored CSS, removed broken analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
-// We are completely removing Analytics for this test to see if it is causing the crash
+// Turning the lights and Tailwind back on!
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
